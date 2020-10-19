b200163e30edd28f5702208d66da8881cda2e96d
Eduardo-Do-Prado <Dudsprado999@gmail.com>
2020-10-19 02:55:34 -0300

    Desafio pronto

---
+++ frontend/src/routers.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './pages/Home/index';
+import Login from './pages/Login/index';
+import Cadastro from './pages/Cadastro/index';
+import Perfil from './pages/Perfil/index';
+import Filmes from './pages/Filmes/index';
+import Genero from './pages/Genero/index';
+import FilmeComum from './pages/FilmesComum/index';
+
+
+function Routers() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/cadastro" component={Cadastro}/>
+      <Route path="/perfil" component={Perfil}/>
+      <Route path="/filmes" component={Filmes}/>
+      <Route path="/genero" component={Genero}/>
+      <Route path="/filmecomum" component={FilmeComum}/>
+    </BrowserRouter>
+  );
+}
+
+export default Routers;
